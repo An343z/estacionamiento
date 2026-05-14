bc095b53ed080b1eb8c767c01adb886004e25af8
gabrielcarzep <gabrielcarzep@gmail.com>
2026-05-14 13:38:13 -0600

    MainView.java

--- src/main/java/com/estacionamiento/ui/main/MainView.java
+++ src/main/java/com/estacionamiento/ui/main/MainView.java
@@ -130,6 +130,7 @@ public class MainView extends BorderPane {
             case PRECIOS          -> new PreciosModule();
             case PROMOCIONES      -> new PromocionesModule();
             case CORREO_RECORDATORIO -> new com.estacionamiento.ui.modules.CorreoRecordatorioView();
+            case USUARIOS         -> new UsuariosModule();
             case NOTIFICACIONES   -> { actualizarBadge(); yield new NotificacionesModule(); }
             case REPORTES         -> new ReportesModule();
             case CONFIGURACION    -> new ConfiguracionModule();
